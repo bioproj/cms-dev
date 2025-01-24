9b18a5e9d2e0c12731f3abdb8fa26ee1b7c9c06b
shanjun <shanjun@mbiolance.com>
2025-01-24 22:24:39 +0800

    update

--- cms-core/src/main/java/com/wangyang/web/controller/user/UserArticleController.java
+++ cms-core/src/main/java/com/wangyang/web/controller/user/UserArticleController.java
@@ -2,7 +2,6 @@ package com.wangyang.web.controller.user;
 
 
 import cn.hutool.core.bean.BeanUtil;
-import com.alibaba.fastjson.JSON;
 import com.wangyang.common.CmsConst;
 import com.wangyang.common.utils.FileUtils;
 import com.wangyang.pojo.annotation.Anonymous;
@@ -128,8 +127,8 @@ public class UserArticleController {
         Article article = articleService.findByIdAndUserId(id, userId);
         ArticleDetailVO articleDetailVO = articleService.conventToAddTags(article);
 //        ArticleDetailVO articleDetailVO = articleService.convert(article);
-        ArticleDetailVO articleDetailVOSimple= BeanUtil.copyProperties(articleDetailVO,ArticleDetailVO.class,"originalContent","formatContent");
-        model.addAttribute("view", JSON.toJSONString(articleDetailVO));
+//        ArticleDetailVO articleDetailVOSimple= BeanUtil.copyProperties(articleDetailVO,ArticleDetailVO.class,"originalContent","formatContent");
+        model.addAttribute("view",articleDetailVO);
 //        model.addAttribute("originalContent",articleDetailVO.getOriginalContent());
 //        model.addAttribute("formatContent",articleDetailVO.getFormatContent());
         return CmsConst.TEMPLATE_FILE_PREFIX+"user/write";
